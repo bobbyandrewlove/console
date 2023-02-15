4a5f458cf44fc6d1429890704297f85b34df9876
Bobby Love <lovebobbyandrew@outlook.com>
2023-02-15 17:10:34 -0600

    first commit

---
+++ console/console/console.ino
@@ -0,0 +1,25 @@
+// Ethan Buerck
+// Bobby Love
+// Ellery Johnsen
+// February 15, 2023
+// GNU GPLv3
+
+// Libraries
+
+
+// Constants
+
+
+// Variables
+
+// Setup Function
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+// Main Function
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
