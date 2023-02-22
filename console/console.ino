889247b24612b60808b14a552e0d16f18d489950
Bobby Love <lovebobbyandrew@outlook.com>
2023-02-22 17:18:10 -0600

    moar tic tac toe

--- console/console.ino
+++ console/console.ino
@@ -8,8 +8,8 @@
 
 
 // Function Prototypes
-int newPieceCheck(int *[][3]);
-int ticTacToeCheck(int [][3]);
+void newPieceCheck(void);
+int ticTacToeCheck(void);
 
 // Constants
 // Pin 0 for Color Mixer button.
@@ -18,11 +18,23 @@ const int cmb = 0;
 const int tttb = 1;
 // Pin 2 for Whack a Mole button.
 const int wamb = 2;
+// Pins 3 to 11 for Tic Tac Toe grid.
+const int zz = 3;
+const int zo = 4;
+const int zt = 5;
+const int oz = 6;
+const int oo = 7;
+const int ot = 8;
+const int tz = 9;
+const int to = 10;
+const int tt = 11;
 
 // Variables
 int cm = 0;
 int ttt = 0;
 int wam = 0;
+// Initialize all indices to 0.
+int board[3][3] = {0};   
 
 // Setup Function
 void setup() {
@@ -44,12 +56,22 @@ void loop() {
     // COLOR MIXER CODE
   // Tic Tac Toe
   } else if (ttt) {
-    // TIC TAC TOE CODE
-    // Initialize all indices to 0.
-    int board[3][3] = {0};    
+    // TIC TAC TOE CODE 
     do {
-      newPieceCheck()        
-    } while (1);
+      // Check the game board for new pieces.
+      newPieceCheck();
+      // Check the game board for a victory.
+      int result = ticTacToeCheck();
+      if (result == 1) {
+        // Print that blue team won.
+        break;
+      } else if (result = 2) {
+        // Print that white team won.
+        break;
+      } else {
+        continue;
+      }
+    } while (1);   
   // Whack a Mole
   } else if (wam) {
     // WHACK A MOLE CODE
@@ -59,7 +81,29 @@ void loop() {
 }
 
 // Function Definitions
-int ticTacToeCheck(int board[][3]) {
+// Checks for new pieces on the gameboard.
+void newPieceCheck(void) {
+  do {
+    int curr_pin = 2;
+    for (int i = 0; i < 3; ++i) {
+      ++curr_pin;
+      for (int j = 0; j < 3; ++j) {
+        // IF BOARD HAS CHANGED, UPDATE THE ARRAY AND STOP LOOPING.
+        if (board[i][j] == 0 && digitalRead(curr_pin) != 0) {
+          board[i][j] == digitalRead(curr_pin);
+          goto done;        
+        }
+        ++curr_pin;
+      }
+    }
+  // IF BOARD IS THE SAME CONTINUE LOOPING.
+  } while (1);
+  done:
+  return;
+}
+
+// Checks for tic tac toe.
+int ticTacToeCheck(void) {
   // Check for all 8 Tic Tac Toe possibilities.
   if (board[0][0] == board[0][1] == board[0][2]) {
     if (board[0][0] != 0) {
